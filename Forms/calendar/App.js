c1c54cb2abe59270448b8614428321caa7756a5f
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-11-23 19:22:13 +0000

    Update App.js

--- Forms/calendar/App.js
+++ Forms/calendar/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Datetime from 'react-datetime';
 
-
 class App extends React.Component {
+constructor (){
+  super()
+  this.state = {
+    fecha:''
+  }
+
+};
+
 render(){
+
   return (
 <div className='App'>
-        <header className='App-header'>
-          <Datetime />
-        </header>
+	<header className='App-header'>
+          <Datetime 
+            onChange={fecha => this.setState({fecha: fecha})}
+          />
+          <button onClick={()=> {
+          alert(this.state.fecha)}
+          }
+          > Enviar </button>
+
+	</header>
 </div>
-  );
+  )
 
 }
-}
+};
 export default App;
-
