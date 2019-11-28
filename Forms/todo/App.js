1f8d20892bfbe20d5cf126181533897ffbbb5266
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-11-28 15:51:26 +0000

    Update App.js

--- Forms/todo/App.js
+++ Forms/todo/App.js
@@ -7,76 +7,95 @@ class  App extends React.Component {
     this.state = {
       data: [],
     }
+    this.handleKeyDown.bind();
+    this.handleChange.bind();
   }
-  render(){
-    const todo =  this.state.data
-    
-    const handleChange = (data) => {
-      this.setState ({data: data.target.value}) 
-      console.log(todo)        
-      }
-      
-      const handleClick = () => {
-        this.setState({data: todo})
-        console.log(todo)
-        alert(todo)
-      }
-      
-      return (
-        
-        <div className="App">
-        <header className="App-header">
-        <div className="Input-box">       
-        <div className="Item">
-        <p>
-        {todo}
-        </p>
-        </div>
-        </div>    
-        <hr></hr>    
-        
-        <div className="Input-Form" > 
-        <form onSubmit={handleChange}>
-        <input 
-        onChange = {e=> handleChange(e) }
-        value = {this.state.data}       
-        >
-        </input>
-        <hr></hr>
-        
-        </form>
-        </div>
-        
-        <div>    
-        <button 
-        className = 'Send-Button'
-        onClick = {handleClick}
-        
-        > 
-        Send 
-        </button>
-        <text> </text>
-        
-        <button 
-        className = 'Save-Button'
-        onClick = ''
-        > 
-        Save
-        </button>
-        
-        <text> </text>
-        
-        <button 
-        className = 'Delete-Button'
-        onClick = {handleChange}
-        > 
-        Delete
-        </button>       
-        </div>
-        </header>  
-        </div>       
-        );
-      }
+  
+  handleKeyDown(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault(); 
+      event.stopPropagation();
     }
-    export default App;
+  }
+  
+  handleChange (data) {
+    this.setState ({data: data.target.value}) ;
+    console.log(data.target.value);
     
+  }
+  
+  render(){
+    
+    const todo = this.state.data;
+  
+    const handleClick = () => {
+      this.setState({data: todo});
+      console.log(todo);
+      alert(todo);
+    }
+    
+    return (
+      
+      <div className="App">
+      <header className="App-header">
+      <div className="Input-box"> 
+      
+      <div className="Item">
+      <p>
+      {todo}
+      </p>
+      </div>  
+      </div>
+      
+      <hr></hr>
+      <div className="Input-Form" > 
+      <form >
+      <input 
+      onChange = {e=> this.handleChange(e)}
+      value = {todo}
+      onKeyDown={this.handleKeyDown}
+      
+      >
+      </input>
+      <hr></hr>
+      
+      </form>
+      </div>
+      
+      <div> 
+      
+      <button 
+      className = 'Send-Button'
+      onClick = {handleClick}
+      
+      > 
+      Send 
+      </button>
+      <text> </text>
+      
+      <button 
+      className = 'Save-Button'
+      onClick = ''
+      > 
+      Save
+      </button>
+      
+      <text> </text>
+      
+      <button 
+      className = 'Delete-Button'
+      onClick = {e=>this.handleChange(e)}
+      > 
+      Delete
+      </button>
+      
+      </div>
+      </header>
+      
+      </div>
+      
+      );
+    }
+  }
+  export default App;
+  
