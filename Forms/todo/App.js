41fadcc82f0414d1f5486cb4479e07113d80a22a
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-11-28 10:30:49 +0000

    Update App.js

--- Forms/todo/App.js
+++ Forms/todo/App.js
@@ -1,44 +1,82 @@
 import React from 'react';
 import './App.css';
 
- class  App extends React.Component {
+class  App extends React.Component {
   constructor (){
     super()
     this.state = {
       data: []
     }
   }
- 
   render(){
     const todo =  this.state.data
     
-  return (
-
-    <div className="App">
-      <header className="App-header">
-       
-       
-        <div className="Ro-name"> 
-     <p>
-     This is the data: {todo}
-     </p>
-
-     </div>
-
-      <div className="Input-Form" > 
+    const handleChange = (data) => {
+      this.setState({data: data.target.value})
+      console.log(todo)
+    }
+    
+    const handleClick = () => {
+      this.setState({data: todo})
+      console.log(todo)
+      alert(todo)
+    }
+    
+    const item = Object.assign (todo)
+    
+    return (
       
-      <input onChange={event=> this.setState({data: event.target.value})
-      }     
-             >
+      <div className="App">
+      <header className="App-header">
+      
+      
+      <div className="Ro-name"> 
+      
+      <p>
+      {todo}
+      </p>
+      
+      <p>
+      {item}
+      </p>
+      
+      </div>
+      
+      <div className="Input-Form" > 
+      <form onSubmit={handleChange}>
+      <input 
+      onChange = { handleChange }
+      value = {this.state.value}
+      
+      >
       </input>
+      </form>
+      </div>
+      
+      <div> 
+      <button 
+      className = 'Send-Button'
+      onClick = {handleClick}
+      
+      > 
+      Send 
+      </button>
+      
+      <button 
+      className = 'Send-Button'
+      onClick = {handleChange}
+      style={{color:'red'}}
+      > 
+      Delete
+      </button>
       
       </div>
       </header>
-     
-   
-    </div>
-   
-  );
-}
- }
-export default App;
+      
+      </div>
+      
+      );
+    }
+  }
+  export default App;
+  
