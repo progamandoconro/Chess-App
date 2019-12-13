6b3518e0cfe2e8a530fdc1ad76ce2c89197253eb
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-12-13 03:29:50 +0000

    Update readDataFirebase.js

--- Firebase/readDataFirebase.js
+++ Firebase/readDataFirebase.js
@@ -1,34 +1,125 @@
-import './App.css';
-import React from 'react'
+import React, { Component} from "react";
 import firebase from 'firebase'
 
-export const FIREBASE_CONFIG = {
+var firebaseConfig = {
   
-  }
-  firebase.initializeApp(FIREBASE_CONFIG)
+  apiKey: "AIzaSyBzkakvvykR5oqjVj1EvtOY2WUN877v38s",
+  authDomain: "webtest-a81dc.firebaseapp.com",
+  databaseURL: "https://webtest-a81dc.firebaseio.com",
+  projectId: "webtest-a81dc",
+  storageBucket: "webtest-a81dc.appspot.com",
+  messagingSenderId: "180737000927",
+  appId: "1:180737000927:web:3d1df2cd0b631a2601b06a",
+  measurementId: "G-9CB63L546Y"
+  
+  
+};
 
-class App extends React.Component{
-  constructor(){
-    super()
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const writeAdminData =(userInfo)=> {
+  firebase.database().ref('user0001').push({
+    userInfo
+  }).catch((error)=>{
+    console.log('error ' , error)
+  })
+}
+
+class HomePage extends Component {
+  
+  constructor() {
+    super();
     this.state = {
-      name:'Carlos'
-    }
+      onListPage: true,
+      data:[],
+      reservas:[],
+      value:  [],
+      
+    };
   }
-
+  
   componentDidMount () {
-    const nameRef = firebase.database().ref().child('object').child('name')
-
-    nameRef.on('value', (snapshot)=> {
-      this.setState({
-        name: snapshot.val()
-      })
-
+    
+    firebase.database().ref('user0001')
+    .on('value', (snapshot)=> {
+      this.setState({reservas:{snapshot}})
     })
   }
-render(){
-  return <h1>Hola {this.state.name}</h1>
-}
-
-}
-
-export default App;
+  
+  componentWillUpdate(){
+    console.log('listening ... ')
+    
+  }
+  
+  render() {
+    
+    const pushAdminData = (data)  => {
+      this.setState({data})
+    }
+    
+    const reservas =  JSON.stringify(this.state.reservas)
+    const tabla =  reservas.split (',').map ((item, i) => 
+    <p key={i}>{item.substr(35).replace("}","").replace("}}","")}</p>)
+    
+    return (
+      
+      <div style={{backgroundColor:'black'}}>
+      <div>
+      
+      <h1 style={{flex:1,textAlign:'center', alignContent:'center'}}>Introduzca la reserva a confirmar:</h1>
+      <div style={myStyle}  
+      >
+      <input 
+      onChange={ e=> this.setState({value: e.target.value})}
+      value={this.state.value} 
+      
+      > 
+      </input>  
+      </div>
+      
+      <div style={myStyle}  
+      >
+      <button
+      onClick= {()=>{
+        pushAdminData(this.state.value) ;
+        writeAdminData( this.state.value) ;
+        
+      }}
+      
+      > Enviar </button>
+      
+      <div  >
+      
+      <li style={{alignItems:'flex-start',color:'white'}}   > { 
+        
+        tabla.reverse()
+        
+      }
+      
+      
+      </li> 
+      
+      
+      </div>
+      </div>           
+      </div>        
+      </div>
+      
+      )
+      
+      
+    }
+    
+  }
+  
+  const myStyle = {
+    
+    flex:1,textAlign:'center', alignContent:'center',alignItems:'center'
+    
+  }
+  
+  export default HomePage;
+  
+  
