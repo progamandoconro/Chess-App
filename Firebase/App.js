18b70d24666673bd7b6971f454da1e00c8dc057b
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-12-13 15:38:24 +0000

    Create App.js

---
+++ Firebase/App.js
@@ -0,0 +1,33 @@
+.myApp {
+  flex: 1;
+  text-align: 'center';
+  align-items: center;
+  align-content: center;
+  background-color: antiquewhite;
+
+}
+
+.myHeader{
+  background-color: #83868b;
+
+}
+
+.mySeparator{
+
+background-color: antiquewhite;
+text-align:center;
+
+}
+
+
+
+.myList {
+  padding: 2%;
+  text-align:left;
+
+  
+  background-color: #29406d;
+  
+  
+
+}
