36b26e10da3e2984fdd077bfb599330adf2ad919
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-01-23 15:45:04 +0000

    Create King.js

---
+++ ChessRandom/King.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import Chess from 'react-chess';
+
+const King = () => {
+
+    const [letter, setLetter] = useState(['b']);
+    const [number, setNumber] = useState(Number(1));
+    const [piece] = useState(['K@']);
+    const [index, setIndex] = useState(Number(1))
+    const [position, handlePosition] = useState(['K@a1']);
+
+    const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+    const makeKingMove = (e) => {
+        const randomMove = () => {
+
+            if (Number(Math.random()) > 0.49) {
+                setNumber(Number(number) + 1)
+            }
+            else {
+                if (Number(Math.random()) > 0.49) {
+                    setIndex(index + 1)
+                }
+                else {
+                    setIndex(index - 1)
+                }
+                setNumber(Number(number) - 1)
+            }
+            if (Number(number) >= 8) {
+                setNumber(Number(number) - 1)
+            }
+            if (Number(number <= 1)) {
+                setNumber(Number(number) + 1)
+            }
+            if (Number(index) >= 7) {
+                setIndex(Number(index) - 1)
+            }
+            if (Number(index) <= 0) {
+                setIndex(Number(index) + 1)
+            }
+
+        }
+        setLetter(letters[Number(index)])
+        randomMove();
+        handlePosition([piece + letter + number]);
+        console.log(position);
+    }
+
+    return (
+        <div >
+            <button onClick={(e) => makeKingMove(e.target.value)} > Move </button>
+            <Chess pieces={position} allowMoves={false} />
+
+        </div>
+    )
+}
+
+export default King;
