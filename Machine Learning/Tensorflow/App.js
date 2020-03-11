b8f6fe07d6134cd83b05afd787fa4169117c8be1
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-03-11 00:49:47 +0000

    Update App.js

--- Machine Learning/Tensorflow/App.js
+++ Machine Learning/Tensorflow/App.js
@@ -1,21 +1,28 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import * as tf from "@tensorflow/tfjs";
 
 function App() {
   const [result, fixResult] = useState([0]);
-
+  const dataX = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const dataY = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
   const myFirstNet = async () => {
     const model = tf.sequential();
-    model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+    model.add(
+      tf.layers.dense({ units: 2, inputShape: [1], activation: "selu" })
+    );
+
+    model.add(tf.layers.dense({ units: 1, activation: "selu" }));
+
     model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
-    const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-    const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+    const xs = tf.tensor2d(dataX, [12, 1]);
+    const ys = tf.tensor2d(dataY, [12, 1]);
 
     const r = async () => {
       model.fit(xs, ys).then(() => {
-        const p = model.predict(tf.tensor2d([5], [1, 1]));
+        const p = model.predict(tf.tensor2d([13], [1, 1]));
         fixResult(p.toString());
       });
     };
@@ -25,13 +32,14 @@ function App() {
 
   const handleResult = async () => {
     await myFirstNet();
-    if (result !== undefined) {
+    if (result === undefined) {
+      console.log("Please wait");
     }
   };
 
   const showResult = () => {
     if (result !== undefined) {
-      return <h1>{result[0]}</h1>;
+      return <h1>{result}</h1>;
     } else {
       return <h1>{"wait"}</h1>;
     }
