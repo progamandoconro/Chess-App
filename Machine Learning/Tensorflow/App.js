4df5ac0d2093fe678b57be6048d19e8e4f97106e
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-03-11 19:19:29 +0000

    Update App.js

--- Machine Learning/Tensorflow/App.js
+++ Machine Learning/Tensorflow/App.js
@@ -5,16 +5,18 @@ import * as tf from "@tensorflow/tfjs";
 
 function App() {
   const [result, fixResult] = useState([0]);
+
+  const [fiboNth, setFiboNth] = useState(Number(1));
   const dataX = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   const dataY = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
   const myFirstNet = async () => {
     const model = tf.sequential();
 
     model.add(
-      tf.layers.dense({ units: 2, inputShape: [1], activation: "selu" })
+      tf.layers.dense({ units: 20, inputShape: [1], activation: "selu" })
     );
 
-    model.add(tf.layers.dense({ units: 1, activation: "selu" }));
+    model.add(tf.layers.dense({ units: 1, activation: "linear" }));
 
     model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
     const xs = tf.tensor2d(dataX, [12, 1]);
@@ -22,7 +24,7 @@ function App() {
 
     const r = async () => {
       model.fit(xs, ys).then(() => {
-        const p = model.predict(tf.tensor2d([13], [1, 1]));
+        const p = model.predict(tf.tensor2d([Number(fiboNth)], [1, 1]));
         fixResult(p.toString());
       });
     };
@@ -49,6 +51,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <input
+          type="number"
+          value={fiboNth}
+          onChange={e => setFiboNth(e.target.value)}
+        />
 
         <button
           onClick={() => {
