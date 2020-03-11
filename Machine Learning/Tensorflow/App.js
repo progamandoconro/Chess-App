6d15190554a5a6ef0ef78b89b9f6d070de1df9c9
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-03-11 21:05:53 +0000

    Update App.js

--- Machine Learning/Tensorflow/App.js
+++ Machine Learning/Tensorflow/App.js
@@ -5,12 +5,27 @@ import * as tf from "@tensorflow/tfjs";
 
 function App() {
   const [result, fixResult] = useState([0]);
+  const [fiboData, setFiboData] = useState([0]);
+  const [showData, setShowData] = useState([0]);
 
   const [fiboNth, setFiboNth] = useState(Number(1));
   const [hidden, setHidden] = useState(Number(1));
 
   const dataX = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  const dataY = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const dataY = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
+
+  const dataFull = () => {
+    if (Number(fiboData) <= 2) setShowData(1);
+    else {
+      const f = [0, 1, 1];
+      for (let i = 3; i <= Number(fiboData); i++) {
+        f[i] = f[i - 1] + f[i - 2];
+      }
+
+      setShowData(f[f.length - 1]);
+    }
+  };
+
   const myFirstNet = async () => {
     const model = tf.sequential();
 
@@ -57,6 +72,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h5>Data Length</h5>
+        <input
+          type="number"
+          value={fiboData}
+          onChange={e => setFiboData(e.target.value)}
+        />
+        {showData}
+
         <h5>Number to predict</h5>
         <input
           type="number"
@@ -77,6 +100,7 @@ function App() {
           onClick={() => {
             handleResult();
             showResult();
+            dataFull();
           }}
         >
           Net Calculate
