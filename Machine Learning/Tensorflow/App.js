c7bf45007abeb86ce71c4fb81e8e7958d1808ff1
Ro <50117686+progamandoconro@users.noreply.github.com>
2020-03-12 20:32:32 +0000

    Update App.js

--- Machine Learning/Tensorflow/App.js
+++ Machine Learning/Tensorflow/App.js
@@ -5,26 +5,12 @@ import * as tf from "@tensorflow/tfjs";
 
 function App() {
   const [result, fixResult] = useState([0]);
-  const [fiboData, setFiboData] = useState([0]);
-  const [showData, setShowData] = useState([0]);
 
-  const [fiboNth, setFiboNth] = useState(Number(1));
+  const [number, setNumber] = useState(Number(1));
   const [hidden, setHidden] = useState(Number(1));
 
   const dataX = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-  const dataY = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
-
-  const dataFull = () => {
-    if (Number(fiboData) <= 2) setShowData(1);
-    else {
-      const f = [0, 1, 1];
-      for (let i = 3; i <= Number(fiboData); i++) {
-        f[i] = f[i - 1] + f[i - 2];
-      }
-
-      setShowData(f[f.length - 1]);
-    }
-  };
+  const dataY = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   const myFirstNet = async () => {
     const model = tf.sequential();
@@ -44,8 +30,8 @@ function App() {
     const ys = tf.tensor2d(dataY, [12, 1]);
 
     const r = async () => {
-      model.fit(xs, ys).then(() => {
-        const p = model.predict(tf.tensor2d([Number(fiboNth)], [1, 1]));
+      model.fit(xs, ys, { epochs: 5 }).then(() => {
+        const p = model.predict(tf.tensor2d([Number(number)], [1, 1]));
         fixResult(p.toString());
       });
     };
@@ -72,19 +58,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h5>Data Length</h5>
-        <input
-          type="number"
-          value={fiboData}
-          onChange={e => setFiboData(e.target.value)}
-        />
-        {showData}
 
         <h5>Number to predict</h5>
         <input
           type="number"
-          value={fiboNth}
-          onChange={e => setFiboNth(e.target.value)}
+          value={number}
+          onChange={e => setNumber(e.target.value)}
         />
 
         <h5>Hidden layer</h5>
@@ -100,7 +79,6 @@ function App() {
           onClick={() => {
             handleResult();
             showResult();
-            dataFull();
           }}
         >
           Net Calculate
