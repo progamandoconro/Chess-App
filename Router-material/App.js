4ff30229f917f7fef07d282fea29094f9e52f94b
Ro <50117686+programandoconro@users.noreply.github.com>
2020-06-05 00:30:41 +0100

    Update App.js

--- Router-material/App.js
+++ Router-material/App.js
@@ -1,31 +1,50 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { Container, Typography, Tab, Tabs } from "@material-ui/core/";
+import { Card, Container, Typography, Tab, Tabs } from "@material-ui/core/";
 import "./App.css";
 
 export default function App() {
   return (
     <Router>
       <Container>
-        <Tabs>
-          <Link to="/">
-            <Tab label="Home"></Tab>
-          </Link>
+        <br />
+        <hr />
+        <Card>
+          <Tabs>
+            <Link to="/">
+              <Tab label="Home"></Tab>
+            </Link>
 
-          <Link to="/about">
-            <Tab label="About"></Tab>
-          </Link>
+            <Link to="/about">
+              <Tab label="About"></Tab>
+            </Link>
 
-          <Link to="/topics">
-            <Tab label="Topics"></Tab>
-          </Link>
-        </Tabs>
+            <Link to="/topics">
+              <Tab label="Topics"></Tab>
+            </Link>
+
+            <Link to="/charts">
+              <Tab label="Charts"></Tab>
+            </Link>
+
+            <Link to="/tables">
+              <Tab label="Tables"></Tab>
+            </Link>
+          </Tabs>
+        </Card>
+        <hr />
         <Switch>
           <Route path="/about">
             <About />
           </Route>
           <Route path="/topics">
             <Topics />
+          </Route>
+          <Route path="/charts">
+            <Charts />
+          </Route>
+          <Route path="/tables">
+            <Tables />
           </Route>
           <Route path="/">
             <Home />
@@ -37,13 +56,46 @@ export default function App() {
 }
 
 function Home() {
-  return <Typography>Home</Typography>;
+  return (
+    <Container>
+      <br />
+      <Typography>Home</Typography>
+    </Container>
+  );
 }
 
 function About() {
-  return <Typography>About</Typography>;
+  return (
+    <Container>
+      <br />
+      <Typography>About</Typography>
+    </Container>
+  );
 }
 
 function Topics() {
-  return <Typography>Topics </Typography>;
+  return (
+    <Container>
+      <br />
+      <Typography>Topics</Typography>
+    </Container>
+  );
+}
+
+function Charts() {
+  return (
+    <Container>
+      <br />
+      <Typography>Charts</Typography>
+    </Container>
+  );
+}
+
+function Tables() {
+  return (
+    <Container>
+      <br />
+      <Typography>Tables</Typography>
+    </Container>
+  );
 }
