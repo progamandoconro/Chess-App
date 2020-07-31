55db853068ed6bcf0f684c79555107dce040fc7c
Ro <50117686+programandoconro@users.noreply.github.com>
2020-07-31 17:34:26 +0200

    Update Clock.js
    Added time in Tenerife and Fukuoka,calculated using UTC time.

--- Clock/Clock.js
+++ Clock/Clock.js
@@ -1,43 +1,65 @@
 import React, { useState, useEffect } from 'react';
+import './App.css';
 
-export default function Clock() {
-  
+function App() {
   const initialTime = new Date();
-  const s_ini = initialTime.getSeconds();
-  const m_ini = initialTime.getMinutes();
-  const h_ini = initialTime.getHours();
+  const s_ini = initialTime.getUTCSeconds();
+  const m_ini = initialTime.getUTCMinutes();
+  const h_ini = initialTime.getUTCHours();
 
   const [seconds, setSeconds] = useState(s_ini);
   const [minutes, setMinutes] = useState(m_ini);
   const [hours, setHours] = useState(h_ini);
 
-  const sleep = (ms) => {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  };
+  // const hoursClock = Array.from(Array(23).keys());
 
-  const updateTime = async () => {
-    await sleep(1000);
+  const updateSeconds = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const currentTime = new Date();
-    const s = currentTime.getSeconds();
-    const m = currentTime.getMinutes();
-    const h = currentTime.getHours();
-    
+    const s = currentTime.getUTCSeconds();
+    const m = currentTime.getUTCMinutes();
+    const h = currentTime.getUTCHours();
     setSeconds(s);
     setMinutes(m);
     setHours(h);
   };
 
   useEffect(() => {
-    updateTime();
+    updateSeconds();
   });
+
+  const timeTenerife = () => {
+    let tenerifeHours = h_ini + 1;
+    h_ini === 23 && (tenerifeHours = 0);
+
+    return tenerifeHours;
+  };
+
+  const timeJapan = () => {
+    let japanHours = h_ini + 9;
+
+    for (let i = 0; i < 9; i++) {
+      h_ini > 14 + i && (japanHours = i);
+    }
+
+    return japanHours;
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>
-          {hours}:{minutes}:{seconds}
+          UTC {hours}:{minutes}:{seconds}
+        </h1>
+        <h1>
+          Tenerife {timeTenerife()}: {minutes}
+        </h1>
+        <h1>
+          Fukuoka {timeJapan()}: {minutes}
         </h1>
       </header>
     </div>
   );
 }
+
+export default App;
