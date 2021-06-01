807205d62a0e9a4cc132975c9bd17f19ac0e8ff1
Ro <50117686+programandoconro@users.noreply.github.com>
2021-06-01 21:06:26 +0900

    Update Storage.tsx

--- Firebase/firestore/Storage.tsx
+++ Firebase/firestore/Storage.tsx
@@ -1,5 +1,9 @@
-import firestore from '@react-native-firebase/firestore';
+import firebase from '@react-native-firebase/app';
+import '@react-native-firebase/firestore';
 
-const reference = firestore().collection('anki').doc("cnGu1MKoKJFZP06JRMi2")
+const storage = firebase
+  .firestore()
+  .collection('anki')
+  .doc('cnGu1MKoKJFZP06JRMi2');
 
-export {reference}
+export {storage}
